2a1e300f6ac8675fd0287b5d7154b13f1e4f4314
Bibhas Acharya <mail@bibhas.com>
2017-12-30 16:34:50 +0545

    Got CIImage filtering working. Now need to try compositing with an image and we're off to the races. Yay!

--- DeltaSubtitleRenderer/DeltaSubtitleRenderer/Source/SubtitleRenderer.mm
+++ DeltaSubtitleRenderer/DeltaSubtitleRenderer/Source/SubtitleRenderer.mm
@@ -3,6 +3,7 @@
 
 #include <atomic>
 #include <iostream>
+#include <AVFoundation/AVFoundation.h>
 #include "utils/cmtime.h"
 #include "utils/compute.h"
 #include "SubtitleRenderer.h"
@@ -16,6 +17,11 @@
   id<SubtitleRendererDelegate> delegate;
   std::atomic<bool> isRendering;
   NSURL *inputFileURL;
+  struct {
+    AVAsset *asset;
+    AVMutableVideoComposition *mutableFilteredComposition;
+    AVAssetExportSession *exportSession;
+  } renderer;
 }
 
 - (id)initWithMP4AtPath:(NSURL *)aFileURL delegate:(id<SubtitleRendererDelegate>)aDelegate {
@@ -27,6 +33,7 @@
       assert(aFileURL != nil && "Input MP4 cannot be nil!");
       return [aFileURL retain];
     });
+    renderer.asset = [[AVAsset assetWithURL:aFileURL] retain];
   }
   return self;
 }
@@ -34,6 +41,8 @@
 - (void)setSubTitle:(NSString *)aSubtitleString from:(CMTime)startTime to:(CMTime)endTime {
   assert(isRendering.load() == false && "A render is in progress! Cannot add subtitles now...");
   std::cout << "Got subtitle : " << [aSubtitleString UTF8String] << " from : " << [NSStringFromCMTime(startTime) UTF8String] << " to : " << [NSStringFromCMTime(endTime) UTF8String] << std::endl; 
+  // Render a CGImage with the caption and store it for now
+  // TODO
 }
 
 - (void)renderToMP4AtPath:(NSURL *)aFileURL {
@@ -44,8 +53,44 @@
       [delegate subtitleRendererDidStartRendering:self];
     });
   }
-  // Setup the renderer innards
-
+  // Begin rendering
+  isRendering.store(true);
+  renderer.mutableFilteredComposition = [AVMutableVideoComposition videoCompositionWithAsset:renderer.asset applyingCIFiltersWithHandler:[self](AVAsynchronousCIImageFilteringRequest *request) {
+    CIFilter *filter = [CIFilter filterWithName:@"CIGaussianBlur"];
+    // Clamp to avoid blurring transparent pixels at the image edges
+    CIImage *source = [request.sourceImage imageByClampingToExtent];
+    [filter setValue:source forKey:kCIInputImageKey];
+    // Vary filter parameters based on video timing
+    Float64 seconds = CMTimeGetSeconds(request.compositionTime);
+    [filter setValue:[NSNumber numberWithFloat:3.0] forKey:kCIInputRadiusKey];
+    // Crop the blurred output to the bounds of the original image
+    CIImage *output = [filter.outputImage imageByCroppingToRect:request.sourceImage.extent];
+    // Provide the filter output to the composition
+    [request finishWithImage:output context:nil];
+  }];
+  renderer.exportSession = COMPUTE(AVAssetExportSession *, {
+    AVAssetExportSession *resp = [[AVAssetExportSession alloc] initWithAsset:renderer.asset presetName:AVAssetExportPreset1280x720];
+    resp.videoComposition = renderer.mutableFilteredComposition;
+    resp.outputURL = aFileURL;
+    resp.outputFileType = AVFileTypeMPEG4;
+    return resp;
+  });
+  [renderer.exportSession exportAsynchronouslyWithCompletionHandler:[self] {
+    switch (renderer.exportSession.status) {
+      case AVAssetExportSessionStatusCompleted: {
+        std::cout << "AVAssetExportSession completed..." << std::endl;
+        break;
+      }
+      case AVAssetExportSessionStatusFailed: {
+        std::cout << "AVAssetExportSession failed..." << std::endl;
+        break;
+      }
+      case AVAssetExportSessionStatusCancelled: {
+        std::cout << "AVAssetExportSession cancelled..." << std::endl;
+        break;
+      }
+    }
+  }];
 }
 
 - (void)dealloc {
